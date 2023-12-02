9e5204836191fb4e406fd6d7bed7e5e819dc46bc
Uliana Seredenko <uliana1331@gmail.com>
2023-12-02 17:50:04 +0200

    check commit

--- src/index.js
+++ src/index.js
@@ -10,8 +10,11 @@ new SlimSelect({
   select: '.breed-select',
 });
 
+select.style.visibility = 'hidden';
+
 fetchBreeds()
   .then(breeds => {
+    select.style.visibility = 'visible';
     loader.style.display = 'none';
 
     const cat = breeds
@@ -24,3 +27,31 @@ fetchBreeds()
     loader.style.display = 'none';
     Notify.failure('Oops! Something went wrong! Try reloading the page!');
   });
+
+select.addEventListener('change', function () {
+  catInfo.innerHTML = '';
+  const selectedBreed = this.value;
+
+  loader.style.display = 'block';
+
+  fetchCatByBreed(selectedBreed)
+    .then(breeds => {
+      loader.style.display = 'none';
+      catData = breeds[0];
+
+      catInfo.innerHTML = `
+    <div><img src="${catData.breeds[0].url}" width="400" alt="${catData.breeds[0].name}"></div>
+    <div>
+    <h3>${catData.breeds[0].name}</h3>
+    <p>${catData.breeds[0].description}</p>
+    <p>${catData.breeds[0].temperament}</p>
+    </div>
+    `;
+
+      catInfo.style.display = 'flex';
+      catInfo.style.gap = '30px';
+    })
+    .catch(error => {
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    });
+});
