bc67ba7c67b75bb5cab0445798e3ac660279014e
Uliana Seredenko <uliana1331@gmail.com>
2024-10-21 17:23:14 +0200

    timer change

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -7,21 +7,11 @@ import 'izitoast/dist/css/iziToast.min.css';
 //Declarations
 const startBtnEl = document.querySelector('button[data-start]');
 const inputEl = document.querySelector('#datetime-picker');
-const dateNow = Date.now();
 const daysEl = document.querySelector('span[data-days]');
 const hoursEl = document.querySelector('span[data-hours]');
 const minutesEl = document.querySelector('span[data-minutes]');
 const secondsEl = document.querySelector('span[data-seconds]');
 let userSelectedDate = '';
-const notifOpt = {
-  message: 'Please choose a date in the future',
-  messageColor: 'black',
-  messageSize: '14px',
-  position: 'topRight',
-  timeout: 4000,
-  color: 'yellow',
-  closeOnClick: true,
-};
 
 //Calendar
 startBtnEl.disabled = true;
@@ -33,7 +23,7 @@ const options = {
   minuteIncrement: 1,
 
   onClose(selectedDates) {
-    if (selectedDates[0].getTime() < dateNow) {
+    if (selectedDates[0].getTime() < Date.now()) {
       iziToast.show(notifOpt);
     } else {
       userSelectedDate = selectedDates[0];
